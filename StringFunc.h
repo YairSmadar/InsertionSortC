316382ad2b5be2af1f0712f893478a2bcfc99dfe
UrielAbergel <myfliper2@gmail.com>
2019-12-16 15:04:18 +0200

    f

--- StringFunc.h
+++ StringFunc.h
@@ -2,11 +2,11 @@
 #define _THIS_
 
 int getline(char s[]);
-// int getword(char w[]);
-// int substring(char* str1,char* str2);
-// int similar(char *s , char *t,int n);
-// void print_lines(char *str);
-// void print_similar_word(char *str);
+int getword(char w[]);
+int substring(char* str1,char* str2);
+int similar(char *s , char *t,int n);
+void print_lines(char *str);
+void print_similar_word(char *str);
 
 
 #endif
